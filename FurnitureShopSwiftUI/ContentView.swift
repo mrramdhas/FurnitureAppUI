5b2354df2ad2c01999549147130a7b8b342d92f0
Ramdhas <ramdhas.d@sigma.se>
2022-08-17 18:04:11 +0530

    Initial Commit

---
+++ FurnitureShopSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FurnitureShopSwiftUI
+//
+//  Created by Ramdhas on 17/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FurnitureShopSwiftUI/FurnitureShopSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureShopSwiftUIApp.swift
+//  FurnitureShopSwiftUI
+//
+//  Created by Ramdhas on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureShopSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
